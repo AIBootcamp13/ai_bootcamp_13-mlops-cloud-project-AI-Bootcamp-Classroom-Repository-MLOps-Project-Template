3798e590a7de16ba88dc4e69ed5db4bb28af93a2
Skylar_ahn <vivalavidaajy@gmail.com>
2025-06-09 10:31:43 +0900

    feat: mlflow main

--- src/main.py
+++ src/main.py
@@ -71,6 +71,85 @@ def run_preprocess():
 # <<< song <<<
 
 # >>> ahn >>> run_train, run_evaluate
+'''
+import mlflow
+
+# MLflow tracking URI 설정
+mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
+print(f"MLflow Tracking URI: {os.getenv('MLFLOW_TRACKING_URI')}")
+
+def run_train(model_name, tuning_max_evals=None):
+    # tuning_max_evals 옵션을 설정하면 하이퍼파라미터 튜닝을 진행, 
+    # 설정하지 않으면 하이퍼파라미터 튜닝 없이 validation 진행.
+    Models.validation(model_name)
+
+    # if wandb add codes.
+
+    # 데이터셋 및 DataLoader 생성
+    apt = read_remote_dataset('s3://mloops2/apt_trade_data.csv')
+    # print(apt.shape)
+    apt = apt_preprocess(apt)
+    # print(apt.shape)
+    apt, folds_idx = train_val_split(
+        df=apt,
+        datetime_col='datetime',
+        n_folds=5,
+        val_months=3
+    )
+    # print(len(folds_idx))
+    fold_datasets = get_dataset(df=apt, folds_index=folds_idx)
+    full_dataset = AptDataset(
+        df=apt, scaler="No", encoders=dict()
+    )
+
+    # mlflow start run
+    with mlflow.start_run():
+        # train and evaluate with folds
+        model_card_class = Models[model_name.upper()].value
+        model_card = model_card_class(
+            early_stopping_rounds=50,
+            random_seed=42
+        )
+        print("model_card_class=",model_card_class)
+        print("model_card.model=", model_card.model)
+        mean_val_score = float('inf')
+        if tuning_max_evals is not None: # hyperparameter tuning
+            model_card, mean_val_score = hyperparameter_tuning(
+                model_card=model_card, fold_datasets=fold_datasets, max_evals=tuning_max_evals
+            )
+        else: # hyperparameter tuning 안 함
+            model_card, mean_val_score = cross_validation(model_card, fold_datasets)
+
+        # full dataset으로 전체 학습
+        # 현재 시점 model_card는 best_param으로 초기화되었고, 학습은 안 된 상태임.
+        model_card.train(train_dataset=full_dataset, val_dataset=None)    
+
+        # mlflow log metric
+        mlflow.log_metric("val_loss", mean_val_score)
+        
+        # mlflow log params
+        mlflow.log_param()
+
+        # mlflow log model
+        if 
+            mlflow.lightgbm.log_model(, artifact_path="apt_model_lgbm")
+        else:
+            mlflow.catboost.log_model(, artifact_path="apt_model_catboost")
+
+
+        ### model_save
+        model_save(
+            model_card=model_card,
+            val_loss=mean_val_score,
+            scaler=full_dataset.scaler,
+            encoders=full_dataset.encoders,
+        )
+        
+'''
+# >>> ahn <<<
+def run_evaluate():
+
+
 
 # <<< ahn <<<
 
